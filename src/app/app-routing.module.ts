8d967945656c8b69111f0596c0f4e54ba3c576ed
Soolan <info@soolan.com>
2022-08-05 09:11:43 +0800

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingComponent} from './landing/landing.component';
 
 const routes: Routes = [
+  {path: '', component: LandingComponent},
   {
-    path: '',
+    path: 'courses',
     loadChildren: () => import('./courses/course.module')
       .then(m => m.CourseModule),
   },
