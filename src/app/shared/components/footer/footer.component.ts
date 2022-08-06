361c782ebdc0f3f0da170ded60f4a6cdb73f7e1c
Soolan <info@soolan.com>
2022-08-06 11:46:04 +0800

    wip

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  socials = SOCIALS;
 
   constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  navigate(label: string): void {
+    const url = this.socials.find(s => s.label.toLowerCase() === label)?.url;
+    window.open(url, '_blank');
   }
-
 }
---
+++ src/app/shared/data/socials.ts
@@ -0,0 +1,12 @@
+import {Social} from '../models/social';
+
+export const Socials: Social[] = [
+  {url: 'https://twitter.com', label: 'twitter'},
+  {url: 'https://facebook.com', label: 'facebook'},
+  {url: 'https://medium.com', label: 'medium'},
+  {url: 'https://tiktok.com', label: 'tiktok'},
+  {url: 'https://linkedin.com', label: 'linkedin'},
+  {url: 'https://youtube.com', label: 'youtube'},
+  {url: 'https://instagram.com', label: 'instagram'},
+  {url: 'https://whatsapp.com', label: 'whatsapp'},
+];
