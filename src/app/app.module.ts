58111f3aca2ee9fc00e7a163607a707b3f4137d0
Soolan <info@soolan.com>
2022-08-28 07:53:39 +0800

    [WTF-LRN-0001] add release notes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,12 +24,14 @@ import { LandingComponent } from './landing/landing.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { ReleasesComponent } from './releases/releases.component';
+import { ReleaseNotesComponent } from './releases/release-notes/release-notes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     ReleasesComponent,
+    ReleaseNotesComponent,
   ],
   imports: [
     BrowserModule,
