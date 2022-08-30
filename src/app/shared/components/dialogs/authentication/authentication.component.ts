082ed972635d1c69346cc629589f77a604d8af09
Soolan <info@soolan.com>
2022-08-30 11:26:49 +0800

    [WTF-LRN-0003] adjust the styles

--- src/app/shared/components/dialogs/authentication/authentication.component.ts
+++ src/app/shared/components/dialogs/authentication/authentication.component.ts
@@ -1,16 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {CrudService} from '../../../services/crud.service';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {PROFILES} from '../../../data/collections';
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.scss']
 })
-export class AuthenticationComponent implements OnInit {
+export class AuthenticationComponent implements AfterViewInit {
+  ui!: firebaseui.auth.AuthUI;
+  uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: (authResult: any, redirectUrl: any) => this.initProfile(authResult, redirectUrl),
+      uiShown: function () {
+        // The widget is rendered.
+        // Hide the loader.
+        // @ts-ignore
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+  provider!: any;
+  message!: string;
+  success = true;
+  constructor(
+    public dialogRef: MatDialogRef<AuthenticationComponent> ,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public auth: AngularFireAuth,
+    private crud: CrudService
+  ) {}
 
-  constructor( public dialogRef: MatDialogRef<AuthenticationComponent> ) { }
+  ngAfterViewInit(): void {
+    if (!this.data.link) {
+      this.ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+      this.ui.start('#firebaseui-auth-container', this.uiConfig);
+    }
 
-  ngOnInit(): void {
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        user.getIdTokenResult()
+          .then(idTokenResult => {
+            this.provider = idTokenResult.signInProvider;
+            console.log(this.provider);
+            // The result will look like  'google.com', 'facebook.com', ...
+          });
+      }
+    }).then().catch();
   }
 
+  initProfile(authResult: any, redirectUrl: any): boolean {
+    // User successfully signed in.
+    // Return type determines whether we continue the redirect automatically
+    // or whether we leave that to developer to handle.
+    console.log(authResult);
+    const profile = this.crud.docRef(PROFILES.path, authResult.user.uid);
+
+    profile.get().then((docSnapshot) => {
+      if (!docSnapshot.exists) {
+        profile.set({
+          display_name: authResult.user.displayName,
+          avatar: authResult.user.photoURL,
+          firstname: '',
+          lastname: '',
+          wallet_address: '',
+          loyalty: 0,
+          achievements: [],
+          timestamps: {
+            created_at: Date.now(),
+            updated_at: Date.now(),
+            deleted_at: 0
+          }
+        }) // create the document
+      }
+    });
+    this.dialogRef.close();
+    return false; // return true if you are redirecting somewhere after successful login
+  }
+
+  linkAccount(provider: string): void {
+    switch (provider){
+      case "google":
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().currentUser?.linkWithPopup(googleProvider)
+          .then(_ => {
+            this.message = "Google linked successfully!";
+          })
+          .catch(error => this.message = "Can not link your Google account. \n" + error);
+        break;
+      case "twitter":
+        const twitterProvider = new firebase.auth.TwitterAuthProvider();
+        firebase.auth().currentUser?.linkWithPopup(twitterProvider)
+          .then(_ => {
+            this.message = "Twitter linked successfully!";
+          })
+          .catch(error => this.message = "Can not link your Twitter account. \n" + error);
+        break;
+      case "email":
+        //ToDo: read the email/password fields properly
+        const credential = firebase.auth.EmailAuthProvider.credential('email', 'password');
+        firebase.auth().currentUser?.linkWithCredential(credential).then().catch();
+        break;
+    }
+  }
 }
--- src/app/shared/data/navigation.ts
+++ src/app/shared/data/navigation.ts
@@ -2,8 +2,10 @@ import {Option} from '../models/navigation';
 
 export const PRODUCTS: string[] = ['game', 'wallet', 'faucet', 'dex','learn'];
 export const PROFILE: Option[] = [
-  {label: 'Login/Register', icon:'login' },
   {label: 'Wallet', icon:'account_balance_wallet' },
   {label: 'Notifications', icon:'notifications' },
-  {label: 'Settings', icon:'settings' }
+  {label: 'Settings', icon:'settings' },
+  {icon: 'logout', label: 'Logout'},
 ];
+
+export const DIALOG_DELAY: string = '100ms';
