7077c455d76a8572638788845d0126888e218f5e
Soolan <info@soolan.com>
2022-08-31 15:08:45 +0800

    [WTF-LRN-0004] generate the profile component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
