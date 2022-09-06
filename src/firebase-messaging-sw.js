6ce4c184b11bf98d7381e7b9569be323597b98fb
Soolan <info@soolan.com>
2022-09-06 18:38:20 +0800

    [WTF-LRN-0005] create, config and add firebase-messageing-swt.js and update the angular.son with new assets

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.9.3/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.9.3/firebase-messaging-compat.js");
+firebase.initializeApp({
+  apiKey: "AIzaSyCtgI_HMRDn5ZFluNnLDEZV6jmM1h15ujk",
+  authDomain: "wtf-pub.firebaseapp.com",
+  projectId: "wtf-pub",
+  storageBucket: "wtf-pub.appspot.com",
+  messagingSenderId: "791303322884",
+  appId: "1:791303322884:web:d8a49b63278f83a92c2451",
+  measurementId: "G-YJS1NWM7B5"
+});
+const messaging = firebase.messaging();
