66fbdcbf389a97db8137fd365ed0a2f4a93f3a52
Soolan <info@soolan.com>
2022-10-22 16:24:00 +0800

    [LRN-0022] remove the flip mechanism

--- src/app/courses/landing/landing.component.ts
+++ src/app/courses/landing/landing.component.ts
@@ -1,24 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {CrudService} from '../../shared/services/crud.service';
-import {COURSES, LESSONS} from '../../shared/data/collections';
+import {COURSES} from '../../shared/data/collections';
 import {map} from 'rxjs';
 import {LEVELS, STATUSES} from '../../shared/data/generic';
 import {Status} from '../../shared/data/enums';
-import {CARD_FLIP} from '../../shared/animations/card-flip';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
-  animations: [CARD_FLIP]
 })
 export class LandingComponent implements OnInit {
   courses!: any;
   lessons!: any;
   id!: string;
   action!: string;
-  flipState = 'front'; // front: course side - back: lessons side
   avatar = '_files/1645928114044';
 
   constructor(private crud: CrudService, private router: Router) { }
@@ -29,7 +26,7 @@ export class LandingComponent implements OnInit {
       map(this.crud.mapId),
     ).subscribe(
       {
-        next: courses => this.courses = courses,
+        next: courses =>  this.courses = courses,
         error: error => console.log(error)
       }
     );
@@ -37,30 +34,6 @@ export class LandingComponent implements OnInit {
 
   get Levels(): string[] {
     return LEVELS;
-  }
-
-  flip(id?: string): void {
-    if (id) {
-      this.id = id;
-      this.initLessons(this.id);
-      this.flipState = 'back';
-    } else {
-      this.flipState = 'front';
-      this.lessons = undefined;
-      this.id = '';
-    }
-  }
-
-  initLessons(id: string): void {
-    LESSONS.path = `${COURSES.path}/${id}/lessons`
-    this.crud.colRefQuery(LESSONS).pipe(
-      map(this.crud.mapId),
-    ).subscribe(
-      {
-        next: lessons => this.lessons = lessons,
-        error: error => console.log(error)
-      }
-    );
   }
 
   navigate(collection: string, id: string): void {
