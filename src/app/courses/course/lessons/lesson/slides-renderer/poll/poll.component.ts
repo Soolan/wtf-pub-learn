92de77415d08ea17521dbcfa01851fa211cb36dd
Soolan <info@soolan.com>
2022-10-26 21:59:34 +0800

    [LRN-0020] calculate the percentages

--- src/app/courses/course/lessons/lesson/slides-renderer/poll/poll.component.ts
+++ src/app/courses/course/lessons/lesson/slides-renderer/poll/poll.component.ts
@@ -3,22 +3,56 @@ import {CrudService} from '../../../../../../shared/services/crud.service';
 import {POLLS} from '../../../../../../shared/data/collections';
 import {Poll} from '../../../../../../shared/models/poll';
 
+export interface Percentage {
+  option: string;
+  percentage: number;
+}
+
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.scss']
+  styleUrls: ['./poll.component.scss', '../slide.scss']
 })
 export class PollComponent implements OnInit {
   @Input() slide: any;
   voted = false;
   poll!: Poll;
+  stringRef = String;
 
   constructor(private crud: CrudService) { }
 
   ngOnInit(): void {
+    this.crud.docRef(POLLS.path, this.slide.id).get()
+      .then(snap => {
+        this.poll = snap.data();
+        this.voted = localStorage.getItem('voted') === 'yes';
+      })
+      .catch()
+    ;
   }
 
   cast(index: number): void {
-    this.crud.set(POLLS.path, this.slide.id, {})
+    this.poll.votes[index].count++;
+    this.poll.timestamps.updated_at = Date.now();
+    this.crud.set(POLLS.path, this.slide.id, this.poll)
+      .then(_ => {
+        this.voted = true;
+        localStorage.setItem('voted', 'yes')
+      })
+      .catch(error => console.log(error))
+    ;
+  }
+
+  get percentages(): Percentage[] {
+    let percentages: Percentage[] = [];
+    const total = this.poll.votes.reduce((vote, {count}) => vote + count, 0);
+    this.poll.votes.forEach(vote => {
+      percentages.push({
+        option: vote.option,
+        percentage: vote.count/total * 100
+      })
+    })
+    console.log(percentages)
+    return percentages;
   }
 }
--- src/app/shared/data/generic.ts
+++ src/app/shared/data/generic.ts
@@ -14,6 +14,7 @@ export const ACTIONS = [
   'match: answers->questions',
   '',
   '',
+  'poll: pick anything',
   'congratulations!'
 ];
 
