3d85ec60863639fdf88a28e97045c111d5005ada
Soolan <info@soolan.com>
2022-10-28 23:15:54 +0800

    [LRN-0022] wrap up

--- src/app/courses/course/lessons/lesson/slides-renderer/footer/footer.component.ts
+++ src/app/courses/course/lessons/lesson/slides-renderer/footer/footer.component.ts
@@ -1,10 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {SlideService} from '../slide.service';
 import {BOUNCE} from '../../../../../../shared/animations/bounce';
 import {STRETCH} from '../../../../../../shared/animations/strech';
 import {ACTIONS} from '../../../../../../shared/data/generic';
 import {SlideHeaderFooter} from '../../../../../../shared/models/slide';
 import {SlideType} from '../../../../../../shared/data/enums';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {CrudService} from '../../../../../../shared/services/crud.service';
+import {PROFILES, PROGRESSES} from '../../../../../../shared/data/collections';
+import firebase from 'firebase/compat';
+import DocumentReference = firebase.firestore.DocumentReference;
+import {LessonProgress, Progress} from '../../../../../../shared/models/profile';
 
 @Component({
   selector: 'app-renderer-footer',
@@ -13,13 +19,41 @@ import {SlideType} from '../../../../../../shared/data/enums';
   animations: [BOUNCE, STRETCH]
 })
 export class FooterComponent implements OnInit {
+  @Input() courseId!: any;
+  @Input() lessonId!: any;
   response!: string;
   width = 0;
   startSlide = false;
   staticSlide = false;
   hintFillInSlide = false;
   ui!: SlideHeaderFooter;
-  constructor(private slideService: SlideService) { }
+  userId!: string | undefined;
+  progressRef!: DocumentReference;
+  progress!: any;
+  lessonProgress!: LessonProgress;
+
+  constructor(
+    private crud: CrudService,
+    private auth: AngularFireAuth,
+    private slideService: SlideService,
+  ) {
+    this.auth.currentUser
+      .then(user => {
+        this.userId = user?.uid;
+        if(this.userId) {
+          this.progressRef = this.crud.docRef(`${PROFILES.path}/${this.userId}/${PROGRESSES.path}`, this.courseId);
+          this.progressRef.get()
+            .then(snap => {
+              this.progress = snap.data();
+              this.lessonProgress = this.progress.lessons.find(l => l.lesson_id == this.lessonId);
+            })
+            .catch()
+          ;
+        }
+      })
+      .catch()
+    ;
+  }
 
   ngOnInit(): void {
     this.slideService.ui.subscribe({
@@ -40,14 +74,14 @@ export class FooterComponent implements OnInit {
 
   setResponse(response: string): void {
     this.response = '';
-    setTimeout( _ => {
+    setTimeout(_ => {
       this.response = response;
     }, 200)
   }
 
   move(forward: boolean): void {
     const index = forward ? this.ui.marker + 1 : this.ui.marker - 1;
-    console.log(index,ACTIONS[this.slideService.slides[index].type], this.slideService.slides[index])
+    console.log(index, ACTIONS[this.slideService.slides[index].type], this.slideService.slides[index])
     this.slideService.next({
       marker: index,
       action: ACTIONS[this.slideService.slides[index].type],
@@ -55,6 +89,10 @@ export class FooterComponent implements OnInit {
       correct: false,
       completed: false
     })
+    if(this.userId) {
+
+
+    }
   }
 
   hint(): void {
