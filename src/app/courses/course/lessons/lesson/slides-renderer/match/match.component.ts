0fff6552c897c8eb2528251710a3e49c1ca51e2b
Soolan <info@soolan.com>
2022-12-02 22:43:20 +0800

    [LRN-0059] find the next index

--- src/app/courses/course/lessons/lesson/slides-renderer/match/match.component.ts
+++ src/app/courses/course/lessons/lesson/slides-renderer/match/match.component.ts
@@ -4,9 +4,10 @@ import {Card} from '../../../../../../shared/models/slide';
 import {SlideService} from '../slide.service';
 
 export interface MatchStatus {
-  question: any;
+  question: string;
   answered: boolean;
 }
+
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
@@ -23,7 +24,7 @@ export class MatchComponent implements OnChanges {
   index = 0;
   questions!: string[];
   answers!: string[];
-  pending!: string[];
+  pending!: MatchStatus[];
   isCompleted = false;
   bottom = 0;
 
@@ -58,11 +59,11 @@ export class MatchComponent implements OnChanges {
     this.matches.forEach(match => {
       this.questions.push(match.question);
       this.answers.push(match.answer);
+      this.pending.push({question: match.question, answered: false});
     })
     // shuffle the questions & answers
     this.answers.sort(() => Math.random() - 0.5);
     this.questions.sort(() => Math.random() - 0.5);
-    this.pending = {...this.questions}; // save a questions copy to pending, not a reference
   }
 
   private initClicks(): void {
@@ -89,17 +90,14 @@ export class MatchComponent implements OnChanges {
     }
   }
 
-  private setIndex(): void {  // [0,1,2,3]
-    if (this.pending.length == 0) {
-      this.markAsCompleted();
-      return;
-    }
-    this.pending.splice(this.index, 1);
-    if(this.pending[this.index]) {
-      this.index ++;
-    } else {
-
-    }
+  private setIndex(): void {
+    this.pending.forEach((item: MatchStatus, index: number) => {
+      if (!item.answered) {   // find the first un answered question and use its index
+        this.index = index;
+        return;
+      }
+      this.markAsCompleted(); // they are all answered
+    })
   }
 
   private markAsCompleted(): void {
