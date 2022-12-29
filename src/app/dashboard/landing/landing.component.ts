10b28a1e8c646acf7add9cb4384573d5319c6380
Soolan <info@soolan.com>
2022-12-29 09:27:04 +0800

    [LRN-0066] implement current/completed courses logic

--- src/app/dashboard/landing/landing.component.ts
+++ src/app/dashboard/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {CrudService} from '../../shared/services/crud.service';
-import {COURSES, PROFILES} from '../../shared/data/collections';
+import {COURSES, P_COURSES, PROFILES} from '../../shared/data/collections';
 import {map} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {Profile} from '../../shared/models/profile';
+import {CurrentService} from '../../shared/services/current.service';
+import {Status} from '../../shared/data/enums';
 
 @Component({
   selector: 'app-landing',
@@ -12,19 +14,17 @@ import {Profile} from '../../shared/models/profile';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  completed = 20;
-  course!: any;
+  currentCourses!: any[];
+  completedCourses!: any[];
   profile!: Profile;
   profileId!: string;
-  progress: any = {
-    active: 'ikYNdVWtUKQ6cyH2fbvT',
-    previous: [],
-    bookmarks: []
-  }
+  completedProfile = 25;
+
   constructor(
     private crud: CrudService,
     public auth: AngularFireAuth,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private current: CurrentService
   ) { }
 
   ngOnInit(): void {
@@ -39,25 +39,27 @@ export class LandingComponent implements OnInit {
     this.crud.docRef(PROFILES.path, this.profileId).get()
       .then(snapShot => {
         this.profile = snapShot.data();
-        this.completed += this.profile.avatar ? 20 : 0;
-        this.completed += this.profile.firstname ? 20 : 0;
-        this.completed += this.profile.lastname ? 20 : 0;
-        this.completed += this.profile.display_name ? 20 : 0;
+        this.completedProfile += this.profile.avatar ? 25 : 0;
+        this.completedProfile += this.profile.firstname ? 15 : 0;
+        this.completedProfile += this.profile.lastname ? 15 : 0;
+        this.completedProfile += this.profile.display_name ? 20 : 0;
       })
       .catch()
     ;
   }
 
   initCourses(): void {
-    let query = COURSES;
-    query.limit = 1;
+    let query = {...P_COURSES};
+    query.limit = 50;
+    query.orderBy =  {field: 'course.inf.updated_at', direction: 'desc'};
     this.crud.colRefQuery(query).pipe(
       map(this.crud.mapId),
     ).subscribe({
-      next: courses => {
-        this.course = courses[0];
-        console.log(this.course);
-      },
+      next: courses => courses.forEach((course: any) => {
+        course.info.status === Status.Retake ?
+          this.completedCourses.push(course) :
+          this.currentCourses.push(course);
+      }),
       error: error => console.error(error)
     });
   }
