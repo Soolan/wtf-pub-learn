d8f183497ccd207ecfe1a65179e699661b948e6a
Soolan <info@soolan.com>
2022-12-29 08:06:30 +0800

    [LRN-0066] init profile and calculate completion

--- src/app/dashboard/landing/landing.component.ts
+++ src/app/dashboard/landing/landing.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {CrudService} from '../../shared/services/crud.service';
-import {COURSES} from '../../shared/data/collections';
+import {COURSES, PROFILES} from '../../shared/data/collections';
 import {map} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {Profile} from '../../shared/models/profile';
 
 @Component({
   selector: 'app-landing',
@@ -10,16 +12,43 @@ import {map} from 'rxjs';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  completed = 75;
+  completed = 20;
   course!: any;
+  profile!: Profile;
+  profileId!: string;
   progress: any = {
     active: 'ikYNdVWtUKQ6cyH2fbvT',
     previous: [],
     bookmarks: []
   }
-  constructor(public auth: AngularFireAuth, private crud: CrudService) { }
+  constructor(
+    private crud: CrudService,
+    public auth: AngularFireAuth,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.profileId = this.route.snapshot.paramMap.get('profileId') || '';
+    if (this.profileId) {
+      this.initProfile();
+      this.initCourses();
+    }
+  }
+
+  initProfile(): void {
+    this.crud.docRef(PROFILES.path, this.profileId).get()
+      .then(snapShot => {
+        this.profile = snapShot.data();
+        this.completed += this.profile.avatar ? 20 : 0;
+        this.completed += this.profile.firstname ? 20 : 0;
+        this.completed += this.profile.lastname ? 20 : 0;
+        this.completed += this.profile.display_name ? 20 : 0;
+      })
+      .catch()
+    ;
+  }
+
+  initCourses(): void {
     let query = COURSES;
     query.limit = 1;
     this.crud.colRefQuery(query).pipe(
@@ -32,5 +61,4 @@ export class LandingComponent implements OnInit {
       error: error => console.error(error)
     });
   }
-
 }
