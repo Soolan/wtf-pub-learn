bfdd69f0c5ee628d55643e20b650f9532a2a8bb7
Soolan <info@soolan.com>
2022-12-31 22:38:08 +0800

    [WAL-0003] add pay option to the lesson model

--- src/app/shared/models/lesson.ts
+++ src/app/shared/models/lesson.ts
@@ -1,10 +1,13 @@
 import {Timestamps} from './timestamps';
+import {PayOption} from './pay-option';
 
 export interface Lesson {
   name: string;
   avatar: string;
   description: string;
   published: boolean;
+  paidLesson: boolean;
+  lessonPayOptions: PayOption[];
   timestamps: Timestamps;
   order: number;
 }
