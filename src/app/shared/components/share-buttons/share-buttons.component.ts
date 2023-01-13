67a08d2ded04fcde3c48d2c111d848bd01877b8d
Soolan <info@soolan.com>
2023-01-13 19:15:51 +0800

    [LRN-0082] add sharing mechanism for twitter and facebook

--- src/app/shared/components/share-buttons/share-buttons.component.ts
+++ src/app/shared/components/share-buttons/share-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-share-buttons',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-buttons.component.scss']
 })
 export class ShareButtonsComponent implements OnInit {
+  @Input() shareUrl!: string;
+  @Input() message!: string;
+  facebook: string = '';
+  twitter: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    // let searchParams = new URLSearchParams();
+    // searchParams.set('url', this.shareUrl);
+
+    this.facebook = `https://www.facebook.com/sharer/sharer.php?u=${this.shareUrl}&t=${this.message}`;
+    this.twitter =
+      `https://twitter.com/intent/tweet?url=${this.shareUrl}%0a&via=soolan&text=${this.message}%0a&hashtags=WinTheFuture,crypto,course,xrpl,NFTCertificate`;
+  }
+
+  share(url: string) {
+    return window.open(url, "_blank");
+  }
 }
