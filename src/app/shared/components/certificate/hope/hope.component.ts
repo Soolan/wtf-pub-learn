a6bea4d38587c588ed8dc2723b147c42a74940cb
Soolan <info@soolan.com>
2023-01-17 22:26:14 +0800

    [LRN-0087] move hope template to its own component

--- src/app/shared/components/certificate/hope/hope.component.ts
+++ src/app/shared/components/certificate/hope/hope.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {Certificate} from '../../../models/certificate';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-hope',
   templateUrl: './hope.component.html',
   styleUrls: ['./hope.component.scss']
 })
-export class HopeComponent implements OnInit {
+export class HopeComponent implements OnInit, AfterViewInit {
+@ViewChild('hope') hope!: ElementRef;
+@ViewChild('output') output!: ElementRef;
 
-  constructor() { }
+@Input() certificate!: Certificate;
+
+  constructor(private renderer: Renderer2) {
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.createPng();
+  }
+
+  createPng(): void {
+    htmlToImage.toPng(this.hope.nativeElement)
+      .then(dataUrl => {
+        this.hope.nativeElement.delete();
+        const img = this.renderer.createElement('img');
+        this.renderer.setAttribute(img, 'src', dataUrl);
+        this.renderer.setAttribute(img, 'width', '400px');
+        this.renderer.appendChild(this.output.nativeElement, img);
+      })
+      .catch(function (error) {
+        console.error('Oh nose!', error);
+      });
+  }
 }
