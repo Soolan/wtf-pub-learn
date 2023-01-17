43f6dd924c5a914494b6b6e2a4a43848e1f46796
Soolan <info@soolan.com>
2023-01-17 22:19:48 +0800

    [LRN-0087] move joy template to its own component

--- src/app/shared/components/certificate/certificate.component.ts
+++ src/app/shared/components/certificate/certificate.component.ts
@@ -1,16 +1,13 @@
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   Input, OnChanges,
   OnInit,
-  Renderer2, SimpleChanges, TemplateRef,
+  SimpleChanges, TemplateRef,
   ViewChild,
   ViewContainerRef
 } from '@angular/core';
 import {Certificate} from '../../models/certificate';
-import * as htmlToImage from 'html-to-image';
-import {toPng, toJpeg, toBlob, toPixelData, toSvg} from 'html-to-image';
 import {CertLayout} from '../../data/enums';
 
 @Component({
@@ -18,59 +15,28 @@ import {CertLayout} from '../../data/enums';
   templateUrl: './certificate.component.html',
   styleUrls: ['./certificate.component.scss']
 })
-export class CertificateComponent implements OnInit, AfterViewInit, OnChanges {
+export class CertificateComponent implements OnInit, OnChanges {
   @ViewChild('outlet', {read: ViewContainerRef}) outletRef!: ViewContainerRef;
   @ViewChild('content', {read: TemplateRef}) contentRef!: TemplateRef<any>;
 
-  @ViewChild('joy') joy!: ElementRef;
   @ViewChild('hope') hope!: ElementRef;
-  @ViewChild('output') output!: ElementRef;
   @Input() certificate!: Certificate;
 
   node!: any;
   layout = CertLayout;
-  constructor(private renderer: Renderer2) {
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    // console.log(changes);
-    if (!changes['certificate'].firstChange) {
+    // if (!changes['certificate'].firstChange) {
       this.reload();
-      this.createPng();
-    }
-  }
-
-  ngAfterViewInit() {
-    this.createPng();
-  }
-
-  createPng(): void {
-    this.node =
-      this.certificate.layout == 0 ? this.hope.nativeElement :
-        this.certificate.layout == 1 ? this.joy.nativeElement : undefined;
-    if (this.node) {
-      htmlToImage.toPng(this.node)
-        .then(dataUrl => {
-          // this.renderer.
-          console.log(this.node);
-          this.outletRef.clear();
-
-          const img = this.renderer.createElement('img');
-          this.renderer.setAttribute(img, 'src', dataUrl);
-          this.renderer.setAttribute(img, 'width', '400px');
-          this.renderer.appendChild(this.output.nativeElement, img);
-        })
-        .catch(function (error) {
-          console.error('Oh nose!', error);
-        });
-    }
+    // }
   }
 
   public reload() {
-    this.renderer.setValue(this.output, '');
     this.outletRef?.clear();
     this.outletRef?.createEmbeddedView(this.contentRef);
   }
--- src/app/shared/components/certificate/joy/joy.component.ts
+++ src/app/shared/components/certificate/joy/joy.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {Certificate} from '../../../models/certificate';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-joy',
   templateUrl: './joy.component.html',
   styleUrls: ['./joy.component.scss']
 })
-export class JoyComponent implements OnInit {
+export class JoyComponent implements OnInit, AfterViewInit {
+  @ViewChild('joy') joy!: ElementRef;
+  @ViewChild('output') output!: ElementRef;
 
-  constructor() { }
+  @Input() certificate!: Certificate;
+
+  constructor(private renderer: Renderer2) {
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.createPng();
+  }
+
+  createPng(): void {
+    htmlToImage.toPng(this.joy.nativeElement)
+      .then(dataUrl => {
+        this.joy.nativeElement.delete();
+        const img = this.renderer.createElement('img');
+        this.renderer.setAttribute(img, 'src', dataUrl);
+        this.renderer.setAttribute(img, 'width', '400px');
+        this.renderer.appendChild(this.output.nativeElement, img);
+      })
+      .catch(function (error) {
+        console.error('Oh nose!', error);
+      });
+  }
 }
